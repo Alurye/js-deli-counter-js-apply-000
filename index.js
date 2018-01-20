308a2484f7bc4ca64fe49c3b9a0f5592cbb146cf
Arthur Lurye <arthurlurye@gmail.com>
2018-01-20 03:52:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function currentLine(katzDeliLine){
   if (katzDeliLine.length === 0) {
     return "The line is currently empty."
   } else {
-    // return "The line is currently: " + counter + ". " + katzDeliLine.join(' ,');
+     return "The line is currently: " + counter + ". " + katzDeliLine.join(' ,');
     var lineUpdate = "";
     var counter = 1;
 
